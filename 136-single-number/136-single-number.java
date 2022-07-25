3877f7faea473b2361465f8acc50c48eecd26490
Rugved Rajandekar <85165132+RugvedR@users.noreply.github.com>
2022-07-25 12:15:39 +0530

    Time: 942 ms (5.01%), Space: 55.1 MB (5.82%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        List<Integer> list = new ArrayList<Integer>();
+        
+        for(int i=0; i< nums.length; i++){
+            if(list.contains(nums[i])){
+                list.remove(new Integer(nums[i]));
+            }
+            else{
+                list.add(nums[i]);
+            }
+        }
+        return list.get(0);
+    }
+}
