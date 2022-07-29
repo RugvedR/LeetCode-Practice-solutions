853b1d12c744bd6f2d2fe2a120f7c0d422dc9122
Rugved Rajandekar <85165132+RugvedR@users.noreply.github.com>
2022-07-29 18:30:33 +0530

    Time: 1 ms (99.95%), Space: 42.8 MB (94.67%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] buildArray(int[] arr){
+
+        int [] ans = new int[arr.length];
+
+        for(int i=0; i<arr.length;i++){
+            ans[i] = arr[arr[i]];
+        }
+
+        return ans;
+
+    }
+}
